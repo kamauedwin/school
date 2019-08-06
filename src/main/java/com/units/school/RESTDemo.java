05d50458f92f9c56d51e771d1e99f73a3002d1ff
Edwin Kamau <edwin.kamau@strathmore.edu>
2019-08-07 00:34:18 +0300

    Strip PIINs

--- src/main/java/com/units/school/RESTDemo.java
+++ src/main/java/com/units/school/RESTDemo.java
@@ -1,11 +1,8 @@
 package com.units.school;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @Component
 public class RESTDemo implements CommandLineRunner {
@@ -14,8 +11,8 @@ public class RESTDemo implements CommandLineRunner {
         RestTemplate restTemplate = new RestTemplate();
         final String uri = "http://10.51.10.111:1000/registrations";
         Student student = new Student();
-        student.setStudentName("Edwin");
-        student.setStudentNumber("056868");
+        student.setStudentName("John");
+        student.setStudentNumber("00000");
 
         Student addedStudent = restTemplate.postForObject(uri,student, Student.class);
         System.out.println(addedStudent.toString());
--- src/main/java/com/units/school/Validate.java
+++ src/main/java/com/units/school/Validate.java
@@ -3,6 +3,8 @@ package com.units.school;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import java.io.Serializable;
 
